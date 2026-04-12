51816a9220b1a1aedbea3be6db5cbf441ddf66b6
Mindi Hu <mindimhu@gmail.com>
2026-04-12 14:31:58 -0400

    Update ScrollToTop.tsx

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,12 +1,11 @@
-import { useEffect } from "react";
+import {useEffect} from "react";
 import { useLocation } from "react-router-dom";
 
 export default function ScrollToTop() {
-  const { pathname } = useLocation();
-
+    const {pathname: urlLink} = useLocation();
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, [pathname]);
+  }, [urlLink]);
 
   return null;
 }
